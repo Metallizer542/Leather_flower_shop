6c6e6fed68a54deea042c0b95d52ce6e50109a77
Kutyrev Daniil <daniil@Kutyrevs-MacBook-Air.local>
2023-01-23 00:01:37 +0400

    add redirect to main page

--- leather_flower_shop/flowers/urls.py
+++ leather_flower_shop/flowers/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
 from . import views
 
 urlpatterns = [
-    path('', views.flowers, name='flowers')
+    path('', views.flowers, name='flowers'),
 ]
--- leather_flower_shop/leather_flower_shop/settings.py
+++ leather_flower_shop/leather_flower_shop/settings.py
@@ -82,16 +82,16 @@ WSGI_APPLICATION = 'leather_flower_shop.wsgi.application'
 #
 # }
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'mydatabase',
-#        'USER': 'mydatabaseuser',
-#        'PASSWORD': 'mypassword',
-#        'HOST': '127.0.0.1',
-#        'PORT': '5432',
-#    }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'flowers_shop',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
